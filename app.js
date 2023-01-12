3ee8fc78ddaa30d1eea43bb482475d0bdf920328
FlorenciaAmado <amadoflorencia8@gmail.com>
2023-01-12 10:01:22 -0300

    agregando formularios

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const port = 3030;
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
 
-app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`))
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`));
 
